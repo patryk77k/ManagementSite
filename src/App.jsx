ee219e2fb40142a7d2392c1523c0f18dc833201c
patryk77k <patryk77.k@gmail.com>
2023-09-16 19:47:10 +0200

    add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <Register />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
