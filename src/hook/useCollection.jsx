d7b22367572a2a74f5d6c97dbad0b4b3f7263029
patryk77k <patryk77.k@gmail.com>
2023-09-23 00:08:59 +0200

    add onSnapshot

--- src/hook/useCollection.jsx
+++ src/hook/useCollection.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { db } from "../firebase/config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 
 export const useCollection = (collectionName) => {
   const [data, setData] = useState([]);
@@ -8,14 +8,16 @@ export const useCollection = (collectionName) => {
 
   useEffect(() => {
     const getData = async () => {
-      const ref = collection(db, collectionName);
-      const response = await getDocs(ref);
-
-      let result = [];
-      response.forEach((doc) => {
-        result.push({ id: doc.id, ...doc.data() });
-      });
-      setData(result);
+      const response = onSnapshot(
+        collection(db, collectionName),
+        (snapshot) => {
+          let result = [];
+          snapshot.forEach((doc) => {
+            result.push({ id: doc.id, ...doc.data() });
+          });
+          setData(result);
+        }
+      );
     };
     getData(collectionName);
   }, []);
