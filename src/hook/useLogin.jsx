6e34878eac92241effb5d72e806fb9144a84ef9c
patryk77k <patryk77.k@gmail.com>
2023-09-28 18:36:09 +0200

    add user login

--- src/hook/useLogin.jsx
+++ src/hook/useLogin.jsx
@@ -1,18 +1,28 @@
-import { auth } from "../firebase/config";
+import { auth, db } from "../firebase/config";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
+import { doc, updateDoc } from "firebase/firestore";
 
 export const useLogin = () => {
   const [err, setErr] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
 
+  const { user } = useAuthContext();
+
   const login = async (email, password) => {
     setErr(null);
     setIsPending(true);
 
     try {
+      const { uid } = user;
+      const userRef = doc(db, "users", uid);
+      console.log(userRef);
+      await updateDoc(userRef, {
+        online: true,
+      });
+      console.log(uid);
       const res = await signInWithEmailAndPassword(auth, email, password);
       dispatch({ type: "LOGIN", payload: res.user });
       setIsPending(false);
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 //styles
 import "./Login.css";
+import { useLogin } from "../../hook/useLogin";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login, err, isPending } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="auth-form">
+      <h2>login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading...
+        </button>
+      )}
+      {err && <p>{err}</p>}
+    </form>
+  );
 };
 
 export default Login;
