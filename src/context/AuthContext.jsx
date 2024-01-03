7c6b49151ac92740fdc51d4296c09d1ab590fe88
Patryk Kardach <patrykkardach@Air-von-Patryk.fritz.box>
2024-01-03 17:08:12 +0100

    add autologin

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,8 +1,12 @@
 import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase/config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase/config"; // Dodane
 
 export const AuthContext = createContext();
+
 const authReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN":
@@ -21,13 +25,35 @@ export const AuthContextProvider = ({ children }) => {
     user: null,
     authIsReady: false,
   });
-  console.log(state);
 
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       dispatch({ type: "AUTH_IS_READY", payload: user });
-      unsub();
     });
+
+    const autoLogin = async () => {
+      try {
+        const res = await signInWithEmailAndPassword(
+          auth,
+          "guest@gmail.com",
+          "123456"
+        );
+
+        // Ustawienie użytkownika jako "online" w bazie danych
+        if (res.user) {
+          const userRef = doc(db, "users", res.user.uid);
+          await updateDoc(userRef, { online: true });
+        }
+
+        dispatch({ type: "LOGIN", payload: res.user });
+      } catch (error) {
+        console.error("Błąd logowania przy starcie strony:", error);
+      }
+    };
+
+    autoLogin();
+
+    return () => unsubscribe();
   }, []);
 
   return (
